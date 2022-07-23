1ea144e03fbe06f5aa056d46d8e2555039576ba4
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-23 21:11:32 +0300

    renders forms component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Form from './components/forms/Form';
 import TestJokes from './components/jokes/TestJokes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestJokes />
+    <Form />
   </React.StrictMode>
 );
