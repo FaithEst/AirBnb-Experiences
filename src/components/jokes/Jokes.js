8ef9a19a5789a24b2fb086e13eba51558d3b7fb9
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-23 11:28:49 +0300

    adds challenge on conditional rendering

--- src/components/jokes/Jokes.js
+++ src/components/jokes/Jokes.js
@@ -7,16 +7,22 @@ function Jokes(props){
      * - Create state `isShown` (boolean, default to `false`)
      * - Add a button that toggles the value back and forth
      */
+
+    /**
+     * Challenge:
+     * - Only display the punchline paragraph if `isShown` is true
+     */
     const [isShown, setIsShown] = React.useState(false);
     function toggle(){
         setIsShown(prevIsShown => !prevIsShown)
+        console.log(isShown)
     }
-    const mode = isShown ? "shown" : "not shown";
+
     return(
         <div className="joke">
             {props.setup && <h3 className="joke-setup">Setup: {props.setup}</h3>}  
-            <p>Punchline: {props.punchline}</p>
-            <button className="joke--button" onClick={toggle}>{mode}</button>
+            {isShown && <p>Punchline: {props.punchline}</p>}
+            <button className="joke--button" onClick={toggle}>Show punchline</button>
             <hr></hr>
         </div>
     );
