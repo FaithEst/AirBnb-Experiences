e969d3418421dee8596ebbc6c699e574b3cbf070
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-25 11:19:57 +0300

    renders airbnb page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Form from './components/forms/Form';
-import TestJokes from './components/jokes/TestJokes';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Form />
+    <App />
   </React.StrictMode>
 );
